a9fdf2a75f4342cf70251da6d34f684b3aec3e8d
Neil Gall <neilgall@mac.com>
2019-08-21 22:56:31 +0100

    Initial commit

---
+++ parsers.py
@@ -0,0 +1,217 @@
+
+from typing import Callable, Generic, Tuple, TypeVar
+
+T = TypeVar('T')
+U = TypeVar('U')
+V = TypeVar('V')
+Unit = Tuple[()]
+
+class Result(Generic[T]):
+	@classmethod
+	def ok(self, value: T, remaining: str) -> "Result[T]":
+		"""Construct a successful result"""
+		r = self()
+		r._tag = self.ok
+		r._value = value
+		r._remaining = remaining
+		return r
+
+
+	@classmethod
+	def err(self, expected: str, actual: str) -> "Result[T]":
+		"""Construct a failed result"""
+		r = self()
+		r._tag = self.err
+		r._expected = expected
+		r._actual = actual
+		return r
+
+
+	def __repr__(self) -> str:
+		if self._tag == Result.ok:
+			return f"ok({self._value}, {self._remaining})"
+		else:
+			return f"err({self._expected}, {self._actual})"
+
+
+	def __eq__(self, other) -> bool:
+		if self.is_ok() and other.is_ok():
+			return self._value == other._value and self._remaining == other._remaining
+		elif self.is_err() and other.is_err():
+			return self._expected == other._expected and self._actual == other._actual
+		else:
+			return False
+
+
+	def is_ok(self) -> bool:
+		return self._tag == Result.ok
+
+
+	def is_err(self) -> bool:
+		return self._tag == Result.err
+
+
+	def map(self, f: Callable[[T], U]) -> "Result[U]":
+		"""
+		Apply f to the value in this result, if successful
+		"""
+		if self.is_ok():
+			return Result.ok(f(self._value), self._remaining)
+		else:
+			return self
+
+
+	def flat_map(self, f: Callable[[T], "Parser[U]"]) -> "Result[U]":
+		"""
+		Apply f to the value and remaining text in this result, if successful
+		"""
+		if self.is_ok():
+			return f(self._value, self._remaining)
+		else:
+			return self
+
+
+	def map_expected(self, f: Callable[[str], str]) -> "Result[T]":
+		"""
+		Map f over the 'expected' message in this result, if not successful
+		"""
+		if self.is_ok():
+			return self
+		else:
+			return Result.err(f(self._expected), self._actual)
+
+
+class Parser(Generic[T]):
+	"""
+	Decorator for functions from str -> Result[T] providing
+	combinator methods
+	"""
+	def __init__(self, parse: Callable[[str], Result[T]]):
+		self._parse = parse
+
+
+	def __call__(self, input: str) -> Result[T]:
+		return self._parse(input)
+
+
+	def map(self, f: Callable[[T], U]) -> "Parser[U]":
+		"""
+		Map 'f' over the result of this parser
+		"""
+		@Parser
+		def _mapped(input: str) -> Result[U]:
+			return self(input).map(f)
+		return _mapped
+
+
+	def flat_map(self, f: Callable[[T, str], "Parser[U]"]) -> "Parser[U]":
+		"""
+		Apply 'f' to the result and remaining text of this parser
+		"""
+		@Parser
+		def _mapped(input: str) -> "Parser[U]":
+			return self(input).flat_map(f)
+		return mapped
+
+
+	def retn(self, v: U) -> "Parser[U]":
+		"""
+		Replace the result of this parser with the constant 'v'
+		"""
+		@Parser
+		def _mapped(input: str) -> Result[U]:
+			return self(input).map(lambda _: v)
+		return _mapped
+
+
+	def __mul__(self, p2: "Parser[U]") -> "Parser[Tuple[T, U]]":
+		"""
+		A parser for the product of T and U where (as a tuple) where
+		the source text for U immediately follows that for T
+		"""
+		def _step2(v1: T, r1: str) -> Result[Tuple[T, U]]:
+			return p2(r1).map(lambda v2: (v1, v2))
+
+		@Parser
+		def _seq(input: str) -> Result[Tuple[T, U]]:
+			return self(input).flat_map(_step2)
+
+		return _seq
+
+
+	def __or__(self, p2: "Parser[T]") -> "Parser[T]":
+		"""
+		A parser for T which tries this parser first, and 'p2' second
+		if this parser fails
+		"""
+		@Parser
+		def _or(input: str) -> Result[T]:
+			r1 = self(input)
+			if r1.is_ok():
+				return r1
+			else: 
+				def _expect(e2: str) -> str:
+					return f'{r1._expected} or {e2}'
+				return p2(input).map_expected(_expect)
+		return _or
+
+
+class ParserRef(Generic[T]):
+	def set(self, p: Parser[T]):
+		self._p = p
+
+	def __call__(self, input: str) -> Result[T]:
+		return self._p(input)
+
+	def get(self) -> Parser[T]:
+		return Parser(self)
+
+
+@Parser
+def the_letter_a(input: str) -> Result[Unit]:
+	"""
+	A parser which returns Unit if the input text begins with 'a'
+	"""
+	if input[0] == 'a':
+		return Result.ok((), input[1:])
+	else:
+		return Result.err("'a'", input)
+
+
+def string(expected: str) -> Parser[str]:
+	"""
+	A parser which returns Unit if the input text begins with 'expected'
+	"""
+	@Parser
+	def _parse(input):
+		if input.startswith(expected):
+			return Result.ok((), input[len(expected):])
+		else:
+			return Result.err(f"'{expected}'", input)
+	return _parse
+
+
+@Parser
+def skip_whitespace(input: str) -> Result[Unit]:
+	"""
+	A parser which skips initial whitespace in the input
+	"""
+	return Result.ok((), input.lstrip(" \n\r\t"))
+
+@Parser
+def integer(input: str) -> Result[int]:
+	"""
+	A Parser which reads integers
+	"""
+	if not input or not input[0].isdigit():
+		return Result.err("an integer", input)
+
+	value = 0
+	pos = 0
+	while pos < len(input) and input[pos].isdigit():
+		value = (value * 10) + (ord(input[pos]) - ord('0'))
+		pos += 1
+
+	return Result.ok(value, input[pos:])
+
+
---
+++ test_parsers.py
@@ -0,0 +1,40 @@
+from parsers import *
+
+def test_result_ok():
+	r = Result.ok(123, "xx")
+	assert r.is_ok()
+
+def test_result_err():
+	r = Result.err("foo", "bar")
+	assert r.is_err()
+
+def test_the_letter_a():
+	assert the_letter_a("a") == Result.ok((), "")
+	assert the_letter_a("aaa") == Result.ok((), "aa")
+	assert the_letter_a("baa") == Result.err("'a'", "baa")
+
+def test_string():
+	assert string("foo")("foo") == Result.ok((), "")
+	assert string("foo")("foobar") == Result.ok((), "bar")
+	assert string("foo")("barfoo") == Result.err("'foo'", "barfoo")
+
+def test_integer():
+	assert integer("123") == Result.ok(123, "")
+	assert integer("123foo") == Result.ok(123, "foo")
+	assert integer("bar") == Result.err("an integer", "bar")
+	assert integer("") == Result.err("an integer", "")
+
+def test_mul():
+	p = the_letter_a * the_letter_a
+	assert p("aa") == Result.ok(((), ()), "")
+	assert p("aab") == Result.ok(((), ()), "b")
+	assert p("ba") == Result.err("'a'", "ba")
+	assert p("ab") == Result.err("'a'", "b")
+
+def test_or_else():
+	p1 = the_letter_a.retn(1)
+	p2 = string("b").retn(2)
+	p = p1 | p2
+	assert p("aa") == Result.ok(1, "a")
+	assert p("ba") == Result.ok(2, "a")
+	assert p("ca") == Result.err("'a' or 'b'", "ca")
